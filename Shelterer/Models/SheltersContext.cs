e0be5d3516399bd4f46e029cb56ebe4e3adb4abe
Rzepka <punix.k@wp.pl>
2014-03-26 03:27:24 +0100

    dbcontext added

---
+++ Shelterer/Models/SheltersContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Shelterer.Models
+{
+    public class SheltersContext : DbContext
+    {
+        public DbSet<MountainRange> MountainRanges { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Type> Types { get; set; }
+        public DbSet<Object> Objects { get; set; }
+
+        public DbSet<History.DoubleHistory> DoubleHistory { get; set; }
+        public DbSet<History.GeoCoordinateHistory> GeoCoordinateHistory { get; set; }
+        public DbSet<History.IntHistory> IntHistory { get; set; }
+        public DbSet<History.StringHistory> StringHistory { get; set; }
+
+        public SheltersContext()
+            : base("DefaultConnection")
+        {
+
+        }
+    }
+}
