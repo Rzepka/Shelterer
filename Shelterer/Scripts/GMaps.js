5cc0e98c3d0c7dded1780504caa4de3986e23246
Rzepka <Rzepka@users.noreply.github.com>
2015-01-17 02:30:14 +0100

    added: set current location

--- Shelterer/Scripts/GMaps.js
+++ Shelterer/Scripts/GMaps.js
@@ -3,47 +3,47 @@
     var marker = null;
     var infoWindow = null;
 
-    function addLatLng(event) {
-        if (marker) marker.setMap(null);
-        marker = new google.maps.Marker({
-            position: event.latLng,
-            draggable: true,
-            title: event.latLng.toString(),
-            map: map
-        });
-        if (event.noUpdate !== true) {
-            $("input#Latitude")[0].value = marker.position.lat();
-            $("input#Longitude")[0].value = marker.position.lng();
-        }
-
-        if (infoWindow) infoWindow.setMap(null);
-        infoWindow = new google.maps.InfoWindow({ content: marker.position.toString() });
-
-        //// UPDATE position in document
-        google.maps.event.addListener(marker, 'click', function () {
-            infoWindow.open(map, marker);
-        });
-        // google.maps.event.addListener(marker, 'dragstart', function() {
-        // });
-        google.maps.event.addListener(marker, 'drag', function () {
-            infoWindow.setContent(marker.position.toString());
-            infoWindow.setPosition(marker.position);
-        });
-         google.maps.event.addListener(marker, 'dragend', function() {
-             // UPDATE position in document
-             $("input#Latitude")[0].value = marker.position.lat();
-             $("input#Longitude")[0].value = marker.position.lng();
-         });
-    }
-
-
     var mapOptions = {
         center: { lat: 50, lng: 18 },
         zoom: 5
     };
 
     var mapdiv = $("#map-container")[0];
+    var infoDiv = $("div#set-location-info")[0];
+
     map = new google.maps.Map(mapdiv, mapOptions);
+
+    function addLatLng(event) {
+        if (marker) marker.setMap(null);
+
+        marker = new google.maps.Marker({
+            position: event.latLng,
+            draggable: true,
+            title: event.latLng.toString(),
+            map: map
+        });
+
+        $("input#Latitude")[0].value = marker.position.lat().toFixed(6);
+        $("input#Longitude")[0].value = marker.position.lng().toFixed(6);
+
+        if (infoWindow) infoWindow.setMap(null);
+        infoWindow = new google.maps.InfoWindow({ content: marker.position.toString() });
+
+        google.maps.event.addListener(marker, 'click', function () {
+            infoWindow.open(map, marker);
+        });
+
+        google.maps.event.addListener(marker, 'drag', function () {
+            infoWindow.setContent(marker.position.toString());
+            infoWindow.setPosition(marker.position);
+        });
+
+        google.maps.event.addListener(marker, 'dragend', function() {
+            $("input#Latitude")[0].value = marker.position.lat().toFixed(6);
+            $("input#Longitude")[0].value = marker.position.lng().toFixed(6);
+         });
+    }
+
     if (mapdiv.className === "editable-map") {
         var lat = $("input#Latitude")[0].value;
         var lng = $("input#Longitude")[0].value;
@@ -58,7 +58,6 @@
         var pos = new google.maps.LatLng(lat, lng);
         marker = new google.maps.Marker({
             position: pos,
-//            position: new google.maps.LatLng(lat, lng),
             title: pos.toString(),
             map: map
         });
@@ -69,7 +68,7 @@
         var lng = $("input#Longitude")[0].value;
         if (lat !== '' && lng !== '') {
             var pos = new google.maps.LatLng(lat, lng);
-            addLatLng({ latLng: pos, noUpdate: true });
+            addLatLng({ latLng: pos });
         }
     });
 
@@ -78,7 +77,41 @@
         var lng = $("input#Longitude")[0].value;
         if (lat !== '' && lng !== '') {
             var pos = new google.maps.LatLng(lat, lng);
-            addLatLng({ latLng: pos, noUpdate: true });
+            addLatLng({ latLng: pos });
         }
     });
+
+    $("button#set-location").click(function () {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition, showError);
+        } else {
+            infoDiv.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    });
+
+    function showPosition(position) {
+        var lat = position.coords.latitude.toFixed(6);
+        var lng = position.coords.longitude.toFixed(6);
+        var pos = new google.maps.LatLng(lat, lng);
+        addLatLng({ latLng: pos });
+    }
+
+    function showError(error) {
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                infoDiv.innerHTML = "User denied the request for Geolocation.";
+                break;
+            case error.POSITION_UNAVAILABLE:
+                infoDiv.innerHTML = "Location information is unavailable.";
+                break;
+            case error.TIMEOUT:
+                infoDiv.innerHTML = "The request to get user location timed out.";
+                break;
+            case error.UNKNOWN_ERROR:
+                infoDiv.innerHTML = "An unknown error occurred.";
+                break;
+        }
+    }
+
+
 });
